6bc187da0b4270852aa03f139e2cfb506477706d
ricardollmelo <106270601+ricardollmelo@users.noreply.github.com>
2022-05-25 14:37:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,11 +3,9 @@ import streamlit as st
 import pandas as pd
 import pyIpeaData as ipea
 import seaborn as sb
-##### Definindo os títulos #####
 st.header('Dashboard Macroeconômico')
 st.markdown(
     '*Os principais indicadores macroeconômicos - IPCA; PIB; BALANÇA COMERCIAL, INPC, DESEMPREGO.*')
-###### Importando dados ######
 selic = sgs.get({'selic': 432}, start='2010-01-01')
 st.title('SELIC')
 st.line_chart(selic)
