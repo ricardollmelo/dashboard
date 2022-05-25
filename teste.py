0da93a9604d0f665c5d5a58ae90a008722f4e259
ricardollmelo <106270601+ricardollmelo@users.noreply.github.com>
2022-05-25 14:48:22 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from bcb import sgs
+bcb = ("banco central")
+##### Definindo os títulos #####
+st.header('Dashboard Macroeconômico')
+st.markdown(
+    '*Os principais indicadores macroeconômicos - IPCA; PIB; BALANÇA COMERCIAL, INPC, DESEMPREGO.*')
+###### Importando dados ######
+selic = sgs.get({'selic': 432}, start='2010-01-01')
+st.title('SELIC')
+st.line_chart(selic)
+st.title('IPCA')
+ipca = sgs.get({'IPCA': 433}, start='2010-01-01')
+st.line_chart(ipca)
+st.title('Taxa de desemprego (%)')
+desem = sgs.get({'Taxa de desemprego (%)': 24369}, start='2012-03-01')
+st.line_chart(desem)
